a99a17304647bd03156a234709181bef67d09d36
neal-o-r <neal@n-o-r.xyz>
2017-07-06 22:13:53 +0100

    update all

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'aireamhan',
   packages = ['aireamhan'], # this must be the same as the name above
-  version = '1.1',
+  version = '1.2',
   description = 'Teanga Ríomhchlárúchain as Gaeilge',
   author = 'Neal Ó Riain',
   author_email = 'neal@n-o-r.xyz',
   url = 'https://github.com/neal-o-r/aireamhan', # use the URL to the github repo
-  download_url = 'https://github.com/neal-o-r/aireamhan/archive/1.1.tar.gz', 
+  download_url = 'https://github.com/neal-o-r/aireamhan/archive/1.2.tar.gz', 
   keywords = ['Irish', 'language', 'programming language'], 
   classifiers = [],
   scripts=['bin/áireamhán']
